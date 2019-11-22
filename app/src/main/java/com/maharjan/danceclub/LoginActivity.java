20d66407b3582943cab94e82ca123eea7adbf9fb
Raja Maharjan <rajamaharjan55@gmail.com>
2019-11-22 10:49:56 +0545

    dashboard

--- app/src/main/java/com/maharjan/danceclub/LoginActivity.java
+++ app/src/main/java/com/maharjan/danceclub/LoginActivity.java
@@ -1,14 +1,15 @@
 package com.maharjan.danceclub;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_login);
     }
 }
